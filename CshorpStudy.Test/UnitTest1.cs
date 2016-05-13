b1b08ec7bb85fac47722ce91e21a6f062e1f7769
zuifengke <yehui1989@qq.com>
2016-05-13 16:11:42 +0800

    ftp上传文件访问测试

--- CshorpStudy.Test/UnitTest1.cs
+++ CshorpStudy.Test/UnitTest1.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Heren.Common.Libraries;
+using Heren.Common.Libraries.Ftp;
+
 namespace CshorpStudy.Test
 {
     [TestClass]
@@ -21,5 +23,23 @@ namespace CshorpStudy.Test
             string s = "2008年6月9日 05:12";
             bool result = GlobalMethods.Convert.StringToDate(s, ref dt);
         }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //日期判断
+            FtpAccess ftpAccess = new FtpAccess();
+            ftpAccess.FtpIP = "192.168.1.63";
+            ftpAccess.FtpPort = 9000;
+            ftpAccess.Password = "medxdb";
+            ftpAccess.UserName = "medxdb";
+            ftpAccess.FtpMode = FtpMode.PASV;
+            ftpAccess.OpenConnection();
+            string szLocalFile = @"D:\svn\MDP\MedDoc\2-SourceCode\Trunk\Debug\Documents\Temp\68360_34130_20160513153221_5116.xml";
+            string szRemoteFile = "/MEDDOC/68360_34130_20160513153221_5116.xml";
+            bool result = ftpAccess.Upload(szLocalFile, szRemoteFile);
+            ftpAccess.CloseConnection();
+        }
+
     }
 }
